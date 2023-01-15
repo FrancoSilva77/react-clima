99301de8e3adcc1ae12cd5b7911467ae8d5f2718
Franco Silva <franco_tigres18@hotmail.com>
2023-01-14 21:54:38 -0600

    Provider Clima add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import AppClima from "./components/AppClima"
+import { ClimaProvider } from './context/ClimaProvider'
 function App() {
   return (
-    <AppClima />
+    <ClimaProvider>
+      <header>
+        <h1>Buscador de clima</h1>
+      </header>
+      <AppClima />
+    </ClimaProvider>
   )
 }
 
